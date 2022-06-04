40175b8b5aa2a43a70c21965fbc41a2de24497e1
noss <54059028+noss4492@users.noreply.github.com>
2022-06-05 00:55:00 +0900

    1-2 end, my ans is ok

--- 1_String/S02CaseConversion.java
+++ 1_String/S02CaseConversion.java
@@ -9,10 +9,9 @@ public class S02CaseConversion {
         }
         return sb.toString();
     }
-    
+
     public static void main(String[] args) {
         S02CaseConversion T = new S02CaseConversion();
-
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();     
         System.out.print(T.solution(str));
