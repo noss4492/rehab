657649a78c3792908421f5e915a0a94851d30867
noss <54059028+noss4492@users.noreply.github.com>
2022-06-05 00:06:56 +0900

    1-1 String complete My ver & Master ver

--- 1_String/_01_FindText.java
+++ 1_String/_01_FindText.java
@@ -1,29 +1,44 @@
+import java.io.CharArrayReader;
 import java.io.IOException;
 import java.util.Scanner;
 
 
 public class _01_FindText{
-
     // by noss
-    public int solution(String str, char ch) {
+    public int solutionMy(String str, char ch) {
         int cnt = 0;
         
         for(char x : str.toCharArray()){
-            System.out.println("x = "+x);
+            //System.out.println("x = "+x);
             if(ch==x){
                 cnt++;
             }
-            System.out.println("cnt = "+cnt);
+            //System.out.println("cnt = "+cnt);
         }
         return cnt;
     }
 
+
+    // modified
+    public int solution(String str, char ch){
+        int cnt = 0;
+        str = str.toUpperCase();
+        ch = Character.toUpperCase(ch);
+
+        for(char x : str.toCharArray()){
+            if(x==ch) cnt++;
+        }
+        return cnt;
+    }
     public static void main(String[] args) throws IOException {
         _01_FindText T = new _01_FindText();
-
+        /*
         Scanner sc = new Scanner(System.in);
         String str1 = sc.next().toLowerCase();
         char ch1 = (char)System.in.read();
+        ch1 = (ch1 > 'Z') ? ch1 : (char)((byte)ch1|0x20);
+        System.out.print(T.solution(str1, ch1));
+        */
         // 영문자 입력
         // char ch1 = sc.next().charAt(0);
         // ch1 = Character.toUpperCase(ch1);
@@ -31,15 +46,13 @@ public class _01_FindText{
         // ch1 = ch1 < 'a' ? (char)((byte)ch1+('a'-'A')) : ch1 ; 
         // ch1 = (ch1 > 'Z') ? ch1 : (char)((byte)ch1|0x20); 
 
-        ch1 = ch1 > 'Z' ? ch1 : (char)(ch1+32);
         
-        System.out.println("str1 : "+str1);
-        System.out.println("ch1 : "+ch1);
-        
+        // 강사분이 제공하는 방향으로 수정
+        Scanner sc = new Scanner(System.in);
+        String str2 = sc.next();
+        char ch2 = sc.next().charAt(0);
 
-        int ans = T.solution(str1, ch1);
-        System.out.println("ans : "+ans); 
-
-        sc.close();
+        System.out.print(T.solution(str2, ch2));
+        //sc.close();
     }
 }
