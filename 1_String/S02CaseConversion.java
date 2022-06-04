1144b79f6c1274030bd75e86e28e960a19e6c61c
noss <54059028+noss4492@users.noreply.github.com>
2022-06-05 01:05:33 +0900

    1-2 ok, my ans + instructor ans

--- 1_String/S02CaseConversion.java
+++ 1_String/S02CaseConversion.java
@@ -1,13 +1,24 @@
 import java.util.Scanner;
 
 public class S02CaseConversion {
-    public String solution(String str){
+    // by noss
+    public String Mysolution(String str){
         StringBuilder sb = new StringBuilder();
         for(char x : str.toCharArray()){
             x = (x > 'Z') ? (char)((byte)x&0x5f) : (char)((byte)x|0x20);
             sb.append(x);
         }
         return sb.toString();
+    }
+
+    // 강사님st, ok 근데 이러면 gc에 부담가지 않나?
+    public String solution(String str){
+        String answer = "";
+        for(char x : str.toCharArray()){
+            if(Character.isLowerCase(x)) answer+=Character.toUpperCase(x);
+            else answer+=Character.toLowerCase(x);
+        }
+        return answer;
     }
 
     public static void main(String[] args) {
